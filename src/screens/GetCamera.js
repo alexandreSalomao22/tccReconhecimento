9bd0ee1fc0b6001f7fbdab0fd79289a76e57a296
Matheus Soares <matheuslol150@gmail.com>
2020-10-21 00:17:07 -0300

    Inserção NovoAcesso

--- src/screens/GetCamera.js
+++ src/screens/GetCamera.js
@@ -1,82 +1,138 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, TouchableOpacity, Alert, StyleSheet } from 'react-native';
+import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 import { Entypo } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
 
-takePicture = () => {
-  const options = { quality: 1, base64: true, fixOrientation: true, 
-    exif: true};
-  if (this.camera) {
-      this.camera.takePictureAsync(options).then(photo => {
-        photo.exif.Orientation = 1;  
-        Alert.alert(
-          'FOTO TIRADA!'
-       )          
-         console.log(photo);            
-      });    
+export default class GetCamera extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasCameraPermission: null,
+      type: Camera.Constants.Type.back,
+    }
+    this.camera = ""
+    this.onCameraPermission = this.onCameraPermission.bind(this)
   }
-};
 
-onPictureSaved = photo => {
-  Alert.alert(
-    'FOTO TIRADA!'
- )
-  console.log(photo);
-} 
-
-export default function GetCamera() {
-  const [hasPermission, setHasPermission] = useState(null);
-  const [type, setType] = useState(Camera.Constants.Type.back);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
-    })();
-  }, []);
-
-  if (hasPermission === null) {
-    return <View />;
+  componentDidMount() {
+    Permissions
+      .askAsync(Permissions.CAMERA)
+      .then(this.onCameraPermission)
   }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+
+  onCameraPermission({ status }) {
+    this.setState({ hasCameraPermission: status === 'granted' })
   }
-  return (
-    <View style={{ flex: 1 }}>
-      <Camera style={{ flex: 1 }} type={type} ref={(ref) => { this.camera = ref }}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'transparent',
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            alignItems: 'flex-end'
-          }}>
-          <TouchableOpacity
-          style={{
-            backgroundColor: '#808080',
-            marginBottom: 20,
-            borderRadius: 10,
-            padding: 10,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center'
-          }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
+
+  takePicture = () => {
+    const options = { quality: 0.2, base64: true, fixOrientation: true, exif: true};
+    if (this.camera) {
+        this.camera.takePictureAsync(options).then(photo => {
+          photo.exif.Orientation = 1;  
+          Alert.alert(
+            'FOTO TIRADA!'
+          )
+          //console.log(photo)
+          this.return(photo);
+          //console.log(photo);    
+          //this.props.navigation.navigate('GetCamera', photo); 
+        });    
+    }
+  };
+
+  onPictureSaved = photo => {
+    Alert.alert(
+      'FOTO TIRADA!'
+    )
+  }
+
+  return = (photo) => {
+    var params = this.props.navigation.state.params;
+    if (photo.base64 != undefined) {
+      params.fotos.push({"foto": photo.base64})
+    }
+    //params.fotos[] = "teste";
+    this.props.navigation.navigate('NovoAcesso', params);
+  }
+
+  sendRegister = async () => {
+    const BASE_URL = "http://192.168.100.5:3000/user/addAcesso";
+
+    const rawResponse = await fetch (`${BASE_URL}`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            'ra' : '',
+            'nome': '',
+            'cpf': '',
+            'fotos': ''
+        })
+    });
+    var content = await rawResponse.json();
+
+    if (content.status != 200) {
+        alert("Erro ao cadastrar usuário. Tente novamente!")
+    } else {
+        alert(content.message)
+        this.props.navigation.navigate('NovoUsuario');
+    }
+  }
+
+  render() {
+    const { hasCameraPermission, type } = this.state
+
+    if (hasCameraPermission === null) {
+      return <View />;
+    }
+
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    
+    return (
+      <View style={{ flex: 1 }}>
+        <Camera style={{ flex: 1 }} type={type} ref={(ref) => { this.camera = ref }}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+              justifyContent: 'space-around',
+              alignItems: 'flex-end'
             }}>
-            <Ionicons name="md-reverse-camera" size={47} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={this.takePicture} style={{backgroundColor: '#696969', marginBottom: 20,  borderRadius: 10,  padding: 10,  flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-            <Entypo name="camera" size={47} color="white" />
-          </TouchableOpacity>  
-        </View>
-      </Camera>
-    </View>
-  );
+            <TouchableOpacity
+            style={{
+              backgroundColor: '#808080',
+              marginBottom: 20,
+              borderRadius: 10,
+              padding: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+              onPress={() => {
+                if (type === Camera.Constants.Type.back) {
+                  this.setState({ type: Camera.Constants.Type.front})
+                } else {
+                  this.setState({ type: Camera.Constants.Type.back})
+                }
+              }}>
+              <Ionicons name="md-reverse-camera" size={47} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.return} style={{backgroundColor: '#696969', marginBottom: 20,  borderRadius: 10,  padding: 10,  flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+              <Entypo name="ccw" size={47} color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.takePicture} style={{backgroundColor: '#696969', marginBottom: 20,  borderRadius: 10,  padding: 10,  flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+              <Entypo name="camera" size={47} color="white" />
+            </TouchableOpacity>  
+          </View>
+        </Camera>
+      </View>
+    );
+  }
 }
--- src/screens/NovoAcesso.js
+++ src/screens/NovoAcesso.js
@@ -1,24 +1,49 @@
 import React, {Component} from 'react'
-import {View, StyleSheet, Text, TouchableOpacity, Image} from 'react-native'
+import {View, StyleSheet, Text, Dimensions, TouchableOpacity, Image, FlatList, Button, Alert} from 'react-native'
 import Input from '../components/Input'
 import unipSuperior from '../../assets/unipSuperior.png'
 import commonStyles from '../commonStyles'
 import {cpfMask} from '../components/mask'
 import { Entypo } from '@expo/vector-icons'; 
-// import GridFotos from '../components/GridFotos'
+import { Camera } from 'expo-camera';
+import GridFotos from '../components/GridFotos'
+let {width} = Dimensions.get('window')
+
+let numberGrid = 2
+let widthGrid = width / numberGrid
 
 export default class NovoAcesso extends Component{
+    state = {
+        nomeCompleto: '',
+        ra: '',
+        cpf: '',
+        fotos: [],
+        nivelAcesso: null
+    }
+
+    componentDidMount() {
+        if (this.props.navigation.state.params) {
+            var params = this.props.navigation.state.params;
+            this.setState({
+                nomeCompleto: params.nomeCompleto,
+                cpf: params.cpf,
+                ra: params.ra,
+                fotos: params.fotos
+            })
+        }
+        //console.log(this.props.navigation.state);
+    }
 
     login = () =>{
     {/* this.props.navigation.navigate('Home'); //navegação do botão */}
     }
 
     goToImg = () =>{
-        this.props.navigation.navigate('GetCamera');
+        this.props.navigation.navigate('GetCamera', this.state);
     }
 
-   /* sendRegister = async () => {
-        const BASE_URL = "http://192.168.100.5:3000/user/addUser";
+    sendRegister = async () => {
+        const BASE_URL = "http://192.168.100.5:3000/user/addAcesso";
 
         const rawResponse = await fetch (`${BASE_URL}`, {
             method: 'POST',
@@ -27,10 +52,10 @@ export default class NovoAcesso extends Component{
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                'funcional' : this.state.funcional,
+                'ra' : this.state.ra,
                 'nome': this.state.nomeCompleto,
                 'cpf': this.state.cpf,
-                'nivel_acesso': this.state.nivelAcesso
+                'fotos': this.state.fotos
             })
         });
         var content = await rawResponse.json();
@@ -43,14 +68,48 @@ export default class NovoAcesso extends Component{
         }
     }
 
-    state = {
-        nomeCompleto: '',
-        funcional: '',
-        cpf: '',
-        nivelAcesso: null
+    alertImage = (item) => e => {
+        Alert.alert(
+            "Remover",
+            "Deseja remover essa imagem?",
+            [
+              {
+                text: "Sim",
+                onPress: () => console.log("Sim")
+              },
+              {
+                text: "Não",
+                onPress: () => console.log("Não")
+              }
+            ],
+            { cancelable: false }
+          );
+        /*const { fotos } = this.state;
+        let i = 0;
+        fotos.forEach(ft => {
+            if (ft.foto == item.foto) {
+                let index = array.indexOf(item.foto);
+                console.log(index)
+            }
+
+            i++;
+        }) */
+        console.log(item);
+        console.log("Chegou aqui")
     }
-*/
+
+    renderItem = ({item}) => {
+        var id = 123;
+        return (
+            <TouchableOpacity onPress={this.alertImage(id)} key={(_, index) => index} >
+                <Image source={{uri: 'data:image/jpeg;base64,'+ item.foto}} style={styles.itemImage}/>
+            </TouchableOpacity>
+        )
+    }
+
     render(){
+        const { fotos } = this.state
+
         return(
             <View style={styles.main}>
                 <View style={styles.cabecalho}>
@@ -58,10 +117,9 @@ export default class NovoAcesso extends Component{
                          <Image style={styles.logoSuperior} source={unipSuperior} />
                 </View>
                 <View style={styles.infoLogin}>
-                    
-                    <Input placeholder="Digite o Nome Completo" />
-                    <Input placeholder="Digite o CPF" />
-                    <Input placeholder="RA / Funcional" />
+                    <Input placeholder="Digite o Nome Completo" value={this.state.nomeCompleto} onChangeText={text => this.setState({nomeCompleto: text})}/>
+                    <Input placeholder="Digite o CPF" value={this.state.cpf} onChangeText={text => this.setState({cpf: text})}/>
+                    <Input placeholder="RA / Funcional" value={this.state.ra} onChangeText={text => this.setState({ra: text})}/>
                     <TouchableOpacity onPress={this.goToImg} style={styles.touchableButton}> 
                         <View style={styles.button}>
                         <Entypo name="camera" size={24} color="black" />
@@ -69,24 +127,30 @@ export default class NovoAcesso extends Component{
                         </View>
                     </TouchableOpacity>
                 </View>
+                <Text style={styles.textoLogo}>Imagens</Text>
                 <View style={styles.rodape}>
-                  
-                    {/*<GridFotos /> 
-                    renderItem={({item})=> ( 
-                                <Image 
-                                source={{uri: item.picture.user}} 
-                                />
-                    )}  */}
+                    <FlatList  
+                        keyExtractor={(_, index) => index} 
+                        numColumns={numberGrid} data={fotos} 
+                        renderItem={this.renderItem} style={{
+                            backgroundColor: 'white',
+                        }}/>
                 </View>
+                <View style={styles.rodape}>
+                    <TouchableOpacity onPress={this.sendRegister} style={styles.touchableButton}> 
+                        <View style={styles.button}>
+                            <Text style={styles.buttonText}>Cadastrar Acesso</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                
             </View>
-            
         )
     }
 
 }
 
 const styles = StyleSheet.create({
-
     logoSuperior:{
         marginLeft: 250,
         marginTop: -40
@@ -165,7 +229,9 @@ const styles = StyleSheet.create({
         shadowRadius: 2.22,
 
         elevation: 5,
+    },
+    itemImage:{
+        width: widthGrid,
+        height: widthGrid
     }
-
-
 })
